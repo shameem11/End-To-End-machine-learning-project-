37dc99608daf93239dd22d5c1426cd87c99859ed
https://github.com/shameem11 <Shameemmon.mk@gmail.com>
2024-07-02 20:23:50 +0400

    Model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,15 +4,14 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-
-
 from src.components.data_transformation import DataTransformation
-
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -47,7 +46,6 @@ class dataIngestion:
              )
 
         except Exception as e:
-
             raise CustomException(e,sys)
         
 if __name__=="__main__":
@@ -56,8 +54,10 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
         
-
+    modelTrainer=ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
+    
 
         
--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,11 @@ import sys
 
 import numpy as np
 import pandas as pd
-
+import pickle
 import dill 
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_obj(file_path, obj):
     try:
@@ -19,3 +20,29 @@ def save_obj(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(x_train,y_train)
+
+            y_train_pred =model.predict(x_train)
+            y_test_pred= model.predict(x_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            return report
+    except Exception as e:
+        raise CustomException(e,sys)
+                                  
+                    
